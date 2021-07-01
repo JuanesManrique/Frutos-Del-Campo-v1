71ec1615b8170bef4d98b1222ccfb8880e1380b0
Michael Rodriguez <maiikol1192@gmail.com>
2021-07-01 15:12:25 -0500

    Cambios

--- js/app.js
+++ js/app.js
@@ -66,14 +66,6 @@ autoplay: {
     delay: 2500,
     disableOnInteraction: false,
 },
-pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-},
-navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-},
 });
 
 // JS PLAY MODAL
