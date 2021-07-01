68fa93ea15f8d3ff0f968afb6892b3f1727ab9a7
Michael Rodriguez <maiikol1192@gmail.com>
2021-06-30 19:46:22 -0500

    Cambios entrantes

--- js/app.js
+++ js/app.js
@@ -4,15 +4,23 @@ const app = new Vue ({
         posicionActual: 0,
         titulo1: ["Quienes somos","Misión","Visión"],
         textos1: ["Frutos del Campo, es una cooperativa de trabajo asociado, constituida en el año 2018 en el municipio de Villavicencio, Departamento del Meta (Colombia). Formada por un grupo de agricultores e ingenieros con más de 15 años de experiencia demostrable en la siembra distribución y comercialización de frutas a nivel nacional e internacional.",
-        "fgjrfghndisvhtosdinhvelnig eisovhoegsdfgvsndfgdfgvsndhfgvnhdsfgnvdsfhgvsdfjkgvdnsfgvn ldsfkl", "Hola mundo"    
+        "Frutos del Campo  dirige sus esfuerzos a comercializar  internacionalmente frutas y vegetales en fresco desde Colombia, para satisfacer las necesidades de consumidores y apoyando a los Productores agrícolas en la comercialización y tecnificación de cultivos.", "Para el 2023 ser una cooperativa líder en producción, transformación, distribución de frutas y verduras a nivel mundial mediante nuevos desarrollos tecnológicos, una cultura innovadora comprometida con la excelencia, la sostenibilidad  y respeto por el medio ambiente."    
     ]
     },
 
     methods:{
-        
-
         mostrarTexto1: function (posicion) {
             this.posicionActual = posicion
+        },
+        atras1:function(){
+            if(this.posicionActual > 0 ){
+                this.posicionActual --
+            }
+        },
+        adelante1: function(){
+            if (this.posicionActual < this.textos1.length -1){
+            this.posicionActual ++
+            }
         }
     }
 })
